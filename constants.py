99bb41e4ba7460920436cf4c2cbf06fee869a0c0
yuvalhq <yuvalhq@gmail.com>
2025-01-23 10:33:32 +0200

    reajusted the weights

--- constants.py
+++ constants.py
@@ -82,9 +82,9 @@ IMAGENET_STYLE_TEMPLATES_SMALL = [
     "a large painting in the style of {}",
 ]
 
-CONCEPT_WEIGHT = 1 # 0.0312 is max abs value of all other weights
-CONCEPT_ID_LEN = 2
+CONCEPT_WEIGHT = 0.0936 # 0.0312 is max abs value of all other weights
+CONCEPT_ID_LEN = 1
 CONCEPT_ZERO_PLACEHOLDER = "{0}"
 CONCEPT_ONE_PLACEHOLDER = "{1}"
 
-USE_SAFETY_CHECKER = False
+USE_SAFETY_CHECKER = True
